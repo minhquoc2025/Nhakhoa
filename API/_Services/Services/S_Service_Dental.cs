77bd5bd56a657d396755ce37335658fcb13e114f
minhquoc2025 <minhquoc.run2025@gmail.com>
2025-05-13 19:19:48 +0700

    .

--- API/_Services/Services/S_Service_Dental.cs
+++ API/_Services/Services/S_Service_Dental.cs
@@ -1,14 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using AgileObjects.AgileMapper;
 using API._Repositories;
 using API._Services.Interfaces;
 using API.DTOs;
 using API.Models;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Metadata;
 
 namespace API._Services.Services
 {
@@ -17,7 +12,7 @@ namespace API._Services.Services
         public S_Service_Dental(IRepositoryAccessor repositoryAccessor) : base(repositoryAccessor)
         {
         }
-        public async Task<OperationResult> Create (ServiceData model)
+        public async Task<OperationResult> Create(ServiceData model)
         {
             var data = new Service
             {
@@ -26,19 +21,45 @@ namespace API._Services.Services
                 service_describe = model.Service_describe
             };
             _repositoryAccessor.Service.Add(data);
-            try{
+            try
+            {
                 await _repositoryAccessor.Save();
-                return new OperationResult(true,"Create Success");
-            } catch (Exception ex)
-{
-    var fullError = $"{ex.Message} -- INNER: {ex.InnerException?.Message}";
-    return new OperationResult(false, fullError);
-}
+                return new OperationResult(true, "Create Success");
+            }
+            catch (Exception ex)
+            {
+                var fullError = $"{ex.Message} -- INNER: {ex.InnerException?.Message}";
+                return new OperationResult(false, fullError);
+            }
         }
-        public async Task<PaginationUtility<ServiceData>> Getdata (PaginationParam pagination, ServiceParam param)
+        public async Task<PaginationUtility<ServiceData>> Getdata(PaginationParam pagination, ServiceParam param)
         {
             var data = await _repositoryAccessor.Service.FindAll().Project().To<ServiceData>().ToListAsync();
             return PaginationUtility<ServiceData>.Create(data, pagination.PageNumber, pagination.PageSize);
+        }
+        public async Task<OperationResult> Delete(int service_id)
+        {
+            var item = _repositoryAccessor.Service.FirstOrDefault(x=>x.service_id == service_id);
+            if (item == null)
+            {
+                return new OperationResult(false, "Item not found");
+            }
+            _repositoryAccessor.Service.Remove(item);
+            await _repositoryAccessor.Save();
+            return new OperationResult(true, "Delete Success");
+        }
+        public async Task<OperationResult> Update(ServiceData model)
+        {
+            var item = _repositoryAccessor.Service.FirstOrDefault(x => x.service_id == model.Service_id);
+            if (item == null)
+            {
+                return new OperationResult(false, "Item not found");
+            }
+            item.service_name = model.Service_name;
+            item.service_describe = model.Service_describe;
+            _repositoryAccessor.Service.Update(item);
+            await _repositoryAccessor.Save();
+            return new OperationResult(true, "Update Success");
         }
     }
 }
